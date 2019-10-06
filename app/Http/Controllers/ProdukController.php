d00dd9fb60efa429feaf1f24156588985c7fd962
nurchalifah <nurcolifah12345@gmail.com>
2019-10-06 12:29:29 +0700

    eps-32

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Produk;
+
+class ProdukController extends Controller
+{
+    public function daftar(Request $req)
+    {
+    	/*Menghubungkan Tabel Produk dengan Kategori*/
+    	$data = Produk::join('kategori','kategori.id','produk.id_kategori')
+    			->where('nama_produk','like',"%{$req->keyword}%")
+    			->select('produk.*','nama_kategori')
+    			->orderBy('updated_at','desc')
+    			->paginate(10);
+
+    	return view('admin.pages.produk.daftar',['data'=>$data]);
+    }
+}
